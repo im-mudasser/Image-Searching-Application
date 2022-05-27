f2ea003823c5e733843b43d2f01bcc77164deac0
Mudasser Shahzad <82773366+im-mudasser@users.noreply.github.com>
2022-05-27 17:50:40 +0500

    Delete Image-Searching-Application directory

--- Image-Searching-Application/src/api/unsplash.js
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-// export default axios.create({
-//   baseUrl: "https://api.unsplash.com",
-//   headers: {
-//     Authorization: "Client-ID XBTp5nH3N3BC7mehoiJcHFuP7Oy1CCt_ittF_pCm8f0",
-//   },
-// });
